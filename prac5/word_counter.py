cd8d4eab9de628c8f203d743119d34277b956070
Magnus Persson <magnus.persson@my.jcu.edu.au>
2018-08-28 12:44:46 +1000

    Add solutions to prac 5

---
+++ prac5/word_counter.py
@@ -0,0 +1,15 @@
+text = input('text: ').lower()
+
+words = text.split(' ')
+words_count = {}
+for word in words:
+    if word not in words_count:
+        words_count[word] = 1
+    else:
+        words_count[word] += 1
+
+length = max(len(word) for word in words)
+
+for word in words_count:
+    print("{0:{2}s}: {1}".format(word, words_count[word], length))
+
